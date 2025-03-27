aa59acfb7c7287a907e00c398e99b21d0646e2dc
nyehozha <138931794+nyehozha@users.noreply.github.com>
2025-03-27 23:38:09 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,7 +12,7 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
+tg_token = config_yaml["tg_token"]
 openai_api_key = config_yaml["openai_api_key"]
 openai_api_base = config_yaml.get("openai_api_base", None)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
