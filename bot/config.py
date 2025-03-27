124c3ac7b9bdd73c61c376c917fe7c70f67e3f68
nyehozha <138931794+nyehozha@users.noreply.github.com>
2025-03-28 00:28:39 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -13,7 +13,6 @@ config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
 tg_token = config_yaml["tg_token"]
-openai_api_key = config_yaml["openai_api_key"]
 openai_api_base = config_yaml.get("openai_api_base", None)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
@@ -22,6 +21,8 @@ return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
 mongodb_uri = config_yaml["mongodb_uri"]
+import os
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
