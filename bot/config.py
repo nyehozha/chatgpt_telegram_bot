53dd74954cd2040a13008fd51e33a17fb0aec182
nyehozha <138931794+nyehozha@users.noreply.github.com>
2025-03-28 00:13:21 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = config_env.get("MONGODB_URI")
+mongodb_uri = config_yaml["mongodb_uri"]
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
