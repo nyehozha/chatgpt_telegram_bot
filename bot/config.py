9d7e8aac0f0391edcdedc7282f4b559221d10b97
nyehozha <138931794+nyehozha@users.noreply.github.com>
2025-03-28 00:34:46 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,6 @@
 import yaml
 import dotenv
+import os
 from pathlib import Path
 
 config_dir = Path(__file__).parent.parent.resolve() / "config"
@@ -13,7 +14,8 @@ config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
 tg_token = config_yaml["tg_token"]
-openai_api_base = config_yaml.get("openai_api_base", None)
+openai_api_key = os.getenv("OPENROUTER_API_KEY")
+openai_api_base = "https://openrouter.ai/api/v1"
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
@@ -21,8 +23,7 @@ return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
 mongodb_uri = config_yaml["mongodb_uri"]
-import os
-openai_api_key = os.getenv("OPENAI_API_KEY")
+
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
